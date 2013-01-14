e128796c1adf9132d3e05db46335a5dc5edb23ca
Brett Sanders <brettrsanders@gmail.com>
2013-01-14 11:51:51 -0800

    Add image upload field to Quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,7 @@
 class Quote < ActiveRecord::Base
-  attr_accessible :book_id, :content, :notes_attributes
+  attr_accessible :book_id, :content, :notes_attributes, :image
   has_many :notes, dependent: :destroy
   belongs_to :book
   accepts_nested_attributes_for :notes, allow_destroy: true
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130114182624) do
+ActiveRecord::Schema.define(:version => 20130114194852) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(:version => 20130114182624) do
     t.integer  "book_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "image"
   end
 
 end
