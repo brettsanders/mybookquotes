2af7e0bb094a17c655acaa0a9843bd28132299ba
Brett Sanders <brettrsanders@gmail.com>
2013-01-14 10:32:11 -0800

    Add file upload field to Book form

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ActiveRecord::Base
-  attr_accessible :author, :pub_date, :title, :quotes_attributes
+  attr_accessible :author, :pub_date, :title, :quotes_attributes, :image
   has_many :quotes, dependent: :destroy
   accepts_nested_attributes_for :quotes, allow_destroy: true
+  mount_uploader :image, ImageUploader
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130104225842) do
+ActiveRecord::Schema.define(:version => 20130114182624) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130104225842) do
     t.date     "pub_date"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "image"
   end
 
   create_table "notes", :force => true do |t|
