f7767e703bfdb6da7e7899ed44dd325c66fde697
Brett Sanders <brettrsanders@gmail.com>
2013-01-14 12:14:03 -0800

    Add image url uploads for quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,5 @@
 class Quote < ActiveRecord::Base
-  attr_accessible :book_id, :content, :notes_attributes, :image
+  attr_accessible :book_id, :content, :notes_attributes, :image, :remote_image_url
   has_many :notes, dependent: :destroy
   belongs_to :book
   accepts_nested_attributes_for :notes, allow_destroy: true
